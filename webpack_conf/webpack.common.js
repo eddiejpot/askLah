2148586c0c543fc4aa3ed8e6d0791d9484873672
eddiejpot <jamesedwardporter@gmail.com>
2021-08-03 15:32:44 +0800

    edit package file for prod env

---
+++ webpack_conf/webpack.common.js
@@ -0,0 +1,31 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      favicon: path.resolve(__dirname, '../../public/ra-logo.png'),
+    }),
+  ],
+  output: {
+    filename: '[name]-[contenthash].bundle.js',
+    path: path.resolve(__dirname, '../../dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
