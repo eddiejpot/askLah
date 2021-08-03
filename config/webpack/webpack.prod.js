bda5ee22457991e5cf192480ae8ff98e08b75dce
eddiejpot <jamesedwardporter@gmail.com>
2021-08-03 16:00:56 +0800

    edit package file for prod env

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -6,7 +6,8 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   entry: {
     // main: ['../../src/index.jsx'],
-    main: ['./src/index.jsx'],
+    // main: ['./src/index.jsx'],
+    main: path.resolve(__dirname, 'src/index.jsx'),
   },
   mode: 'production',
   stats: {
@@ -17,7 +18,8 @@ module.exports = merge(common, {
       // name this file main, so that it does not get automatically requested as a static file
       filename: 'main.html',
       // inject: true,
-      template: path.resolve(__dirname, '..', 'src', 'index.html'),
+      template: path.resolve(__dirname, 'src/index.html'),
+      // template: path.resolve(__dirname, '..', 'src', 'index.html'),
       // a favicon can be included in the head. use this config to point to it
       // favicon: resolve(__dirname, '..', 'src', 'favicon.png'),
       // alwaysWriteToDisk: true,
